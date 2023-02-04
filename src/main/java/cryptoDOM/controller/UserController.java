5ac4dbd5eb98ecce5e6f218af56872c1df00bbcd
danpinkB <belozarovichdanila08@gmail.com>
2023-02-04 19:54:26 +0100

    login like a child

--- src/main/java/cryptoDOM/controller/UserController.java
+++ src/main/java/cryptoDOM/controller/UserController.java
@@ -1,22 +1,25 @@
 package cryptoDOM.controller;
 
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import cryptoDOM.dto.UserDtoRequest;
 import cryptoDOM.dto.UserDtoResponse;
 import cryptoDOM.service.UserService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-@RequestMapping("/registration")
+@RequestMapping("")
 public class UserController {
 
     private final UserService userService;
@@ -25,23 +28,56 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping
+    @GetMapping(value = "/registration")
     public String userRegistration(Model model) {
         return "registration2";
     }
 
-    @PostMapping(consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-    public String userRegistration(UserDtoRequest userDto, Model model) {
+    @PostMapping(value = "/registration")
+    public RedirectView userRegistration(UserDtoRequest userDto,Model model) {
+
         userService.saveUser(userDto);
         List<UserDtoResponse> users = userService.findAll();
         model.addAttribute("users", users);
-        return ("allUsers");
+        return new RedirectView("allUsers");
     }
 
-    @GetMapping("/showAll")
-    public String showAllUsers(Model model) {
-        List<UserDtoResponse> users = userService.findAll();
-        model.addAttribute("users", users);
+    @GetMapping("/allUsers")
+    public String showAllUsers(Model model, HttpServletRequest request) {
+        var session = request.getSession();
+        var attr = session.getAttribute("valid");
+        if(attr!=null&&Boolean.parseBoolean(attr.toString()) ) {
+            List<UserDtoResponse> users = userService.findAll();
+            model.addAttribute("users", users);
+        }
+        else {
+            model.addAttribute("users", new ArrayList<>());
+        }
         return "allUsers";
+    }
+    @GetMapping(value = "/login")
+    public String loginPage(Model model) {
+        return "login";
+    }
+
+    @GetMapping(value = "/logout")
+    public String loginPage(HttpServletRequest request) {
+        var session = request.getSession();
+        session.invalidate();
+        return "logout";
+    }
+    @PostMapping(value = "login")
+    public RedirectView login(HttpServletRequest request, UserDtoRequest userDto){
+        var session = request.getSession();
+        if(userService.isValidEmailAndPassword(userDto.getEmail(),userDto.getPassword()));
+        session.setAttribute("valid",true);
+        return new RedirectView("allUsers");
+    }
+
+    @GetMapping(value = "/test")
+    public ResponseEntity<Object> test(){
+        ObjectNode obj =(ObjectNode) JsonNodeFactory.instance.objectNode();
+        obj.put("sasi","xui");
+        return new ResponseEntity<>(obj, HttpStatus.OK);
     }
 }
--- src/main/java/cryptoDOM/service/UserService.java
+++ src/main/java/cryptoDOM/service/UserService.java
@@ -4,6 +4,7 @@ import cryptoDOM.dto.UserDtoRequest;
 import cryptoDOM.dto.UserDtoResponse;
 import cryptoDOM.mapper.UserMapper;
 import cryptoDOM.model.User;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import cryptoDOM.repository.UserRepository;
@@ -12,10 +13,11 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
-    @Autowired
-    private UserRepository userRepository;
+
+    private final UserRepository userRepository;
 
     public UserDtoResponse saveUser(UserDtoRequest userDto) {
         return UserMapper.fromEntity(
@@ -31,6 +33,9 @@ public class UserService {
 
     public List<UserDtoResponse> findAll() {
         return userRepository.findAll().stream().map(UserMapper::fromEntity).collect(Collectors.toList());
+    }
+    public boolean isValidEmailAndPassword(String email,String password){
+        return userRepository.findByEmailAndPassword(email,password).isPresent();
     }
 
     public void deleteUser(Long id) {
